d44f111f2859cf5002dd4467aa640b08f3d5a9de
niknows <33270711+niknows@users.noreply.github.com>
2018-06-15 22:56:36 -0300

    Working reset button

--- score_keeper/script.js
+++ score_keeper/script.js
@@ -1,10 +1,11 @@
-/*selecting objects*/
+/*--SELECTION--*/
 var playingTo = document.querySelector("input[type='number']");
 var playingToText = document.querySelector(".playingTo");
 var playerOneScore = document.querySelector(".playerOne");
 var playerTwoScore = document.querySelector(".playerTwo");
 var p1Button = document.querySelector(".p1Button");
 var p2Button = document.querySelector(".p2Button");
+var resetButton = document.querySelector(".reset");
 //console.log(p1Button,p2Button);
 
 
@@ -24,3 +25,19 @@ playerOneScore.innerText = Number(playerOneScore.innerText) + 1;
 p2Button.addEventListener('click',function(){
 playerTwoScore.innerText = Number(playerTwoScore.innerText) + 1;
 });
+
+/*Reset button event*/
+resetButton.addEventListener('click',function(){
+console.log("clicked");
+reset();   
+});
+
+
+
+/*--FUNCTIONS--*/
+
+function reset(){
+    playerOneScore.innerText = 0;
+    playerTwoScore.innerText = 0;
+    playingTo.value = 0;
+}
